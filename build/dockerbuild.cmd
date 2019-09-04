e00a72ecc68d5d2c751613e74074e64b735824cc
skunklab <theskunklab@gmail.com>
2019-09-04 17:06:22 -0500

    update vs2019

--- build/dockerbuild.cmd
+++ build/dockerbuild.cmd
@@ -1,5 +1,3 @@
-powershell ./removefiles.ps1
-
 msbuild ..\src\Piraeus.SiloHost\Piraeus.SiloHost.csproj /t:Clean,Rebuild,restore /p:OutputPath=..\..\build\Piraeus.SiloHost\Output /p:Configuration=Release /fl1 /fl2 /fl3 /flp1:logfile=.\BuildOutput\Piraeus.SiloHost.log_errors;errorsonly /flp2:logfile=.\BuildOutput\Piraeus.SiloHost_warnings.log;warningsonly /flp3:logfile=.\BuildOutput\Piraeus.SiloHost.log
 
 msbuild ..\src\Piraeus.WebApi\Piraeus.WebApi.csproj /t:Clean,Rebuild,restore /p:OutputPath=..\..\build\Piraeus.WebApi\Output /p:Configuration=Release /fl1 /fl2 /fl3 /flp1:logfile=.\BuildOutput\Piraeus.WebApi.log_errors;errorsonly /flp2:logfile=.\BuildOutput\Piraeus.WebApi_warnings.log;warningsonly /flp3:logfile=.\BuildOutput\Piraeus.WebApi.log
@@ -14,24 +12,10 @@ dotnet publish "..\src\Piraeus.WebApi\Piraeus.WebApi.csproj" -c Release -o "..\.
 dotnet publish "..\src\Piraeus.WebSocketGateway\Piraeus.WebSocketGateway.csproj" -c Release -o "..\..\build\Piraeus.WebSocketGateway-Out"
 
 
-docker rmi skunklab/piraeus-silo
-docker rmi skunklab/piraeus-mgmt-api
-docker rmi skunklab/piraeus-websocket-gateway
-docker rmi piraeus-silo
-docker rmi piraeus-mgmt-api
-docker rmi piraeus-websocket-gateway
-
 
 docker build -t piraeus-silo ./Piraeus.SiloHost-Out
 docker build -t piraeus-mgmt-api ./Piraeus.WebApi-Out
 docker build -t piraeus-websocket-gateway ./Piraeus.WebSocketGateway-Out
-
-
-
-docker tag piraeus-silo skunklab/piraeus-silo
-docker tag piraeus-mgmt-api skunklab/piraeus-mgmt-api
-docker tag piraeus-websocket-gateway skunklab/piraeus-websocket-gateway
-
 
 
 
